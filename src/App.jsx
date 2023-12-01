075db3ae37ad2c2e368ba59ed37175feef07ce73
maurolps <boroengenharia@gmail.com>
2023-12-01 18:02:39 -0300

    Added card collapse effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import { Personal } from "./components/Personal.jsx"
 import { Experience } from "./components/Experience.jsx"
 import './App.css'
+import { useState } from "react"
 
 function App() {
+  const [collapse, setCollapse] = useState(false);
+
   return (
     <>
       <p>CV Application</p>
       <div className="container">
         <div className="card">
-          <p>Experience</p>
-          <Experience />
+          <div className="card-title" onClick={() => setCollapse(!collapse)}>
+            <p>Experience</p><p>+</p>
+          </div>
+          { collapse && <Experience /> }
+          
         </div>
         <div className="card">
           <p>Personal</p>
