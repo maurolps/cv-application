dc575e0aa7f83796a02e7c8bfbf1980ec5d866f6
maurolps <boroengenharia@gmail.com>
2023-12-05 20:48:15 -0300

    Live preview update

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -1,13 +1,18 @@
 export function Preview(props) {
 
+  const loadPreview = (id) => {
+    const fieldText = document.getElementById(id)
+    if ( fieldText !== null ) return fieldText.value;
+  }
+
   return (
     <>
       <p>Preview</p>
       <h3>Personal Details</h3>
-      <p>{(props.update.id == 'full-name' && props.update.text) || 'Full Name'}</p>
-      <p>{(props.update.id == 'email' && props.update.text)  || 'Email'}</p>
-      <p>{(props.update.id == 'phone-number' && props.update.text) || 'Phone Number'}</p>
-      <p>{(props.update.id == 'address' && props.update.text) || 'Address'}</p>
+      <p>{(props.update.id == 'full-name' && props.update.text) || loadPreview('full-name')}&nbsp;</p>
+      <p>{(props.update.id == 'email' && props.update.text)  || loadPreview('email')}&nbsp;</p>
+      <p>{(props.update.id == 'phone-number' && props.update.text) || loadPreview('phone-number')}&nbsp;</p>
+      <p>{(props.update.id == 'address' && props.update.text) || loadPreview('address')}&nbsp;</p>
       <h3>Work Experience</h3>
       <h3>Education</h3>
     </>
