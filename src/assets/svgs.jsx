b1dd33333318b1ddc75e4633ca174e530953f7a4
maurolps <boroengenharia@gmail.com>
2023-12-08 20:57:42 -0300

    UI Update

--- src/assets/svgs.jsx
+++ src/assets/svgs.jsx
@@ -1,5 +1,5 @@
 const svgPersonal = 
-<svg style={{width: '30px'}} fill="" xmlns="http://www.w3.org/2000/svg" 
+<svg style={{width: '30px'}} fill="#00ffff" xmlns="http://www.w3.org/2000/svg" 
 viewBox="0 0 24 24"><title>Personal</title>
 <path d="M12 3C14.21 3 16 4.79 16 7S14.21 11 12 11 8 9.21 8 7 9.79 3 12
   3M16 13.54C16 14.6 15.72 17.07 13.81 19.83L13 15L13.94 13.12C13.32 13.05
@@ -8,14 +8,14 @@ viewBox="0 0 24 24"><title>Personal</title>
 </svg>
 
 const svgEdu = 
-<svg style={{width: '25px'}} fill="" xmlns="http://www.w3.org/2000/svg" 
+<svg style={{width: '25px'}} fill="#00ffff" xmlns="http://www.w3.org/2000/svg" 
 viewBox="0 0 24 24"><title>Personal</title>
 <path d="M12,3L1,9L12,15L21,10.09V17H23V9M5,13.18V17.18L12,21L19,
   17.18V13.18L12,17L5,13.18Z"  />
 </svg>
 
 const svgExp = 
-<svg style={{width: '25px'}} fill="" xmlns="http://www.w3.org/2000/svg" 
+<svg style={{width: '25px'}} fill="#00ffff" xmlns="http://www.w3.org/2000/svg" 
 viewBox="0 0 24 24"><title>Personal</title>
 <path d="M10,2H14A2,2 0 0,1 16,4V6H20A2,2 0 0,1 22,8V19A2,2 0 0,1 20,21H4C2.89,
 21 2,20.1 2,19V8C2,6.89 2.89,6 4,6H8V4C8,2.89 8.89,2 10,2M14,6V4H10V6H14Z" />
