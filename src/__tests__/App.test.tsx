f04ca8dde6107aee523f1062e68a8ba196fbebdd
maurolps <boroengenharia@gmail.com>
2025-04-09 16:14:27 -0300

    test(Integration): preview should update when input fields change

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -36,3 +36,19 @@ describe("Card Collapse Behavior", () => {
     expect(workExpandedCard).toBeInTheDocument();
   });
 });
+
+describe("Inputs Behavior and Preview Updates", () => {
+  test("Preview should update when input fields change", async () => {
+    render(<App />);
+    const user = userEvent.setup();
+    const nameInput = screen.getByPlaceholderText(/Name/i);
+
+    await user.type(nameInput, "Mauro");
+
+    const previewName = await screen.findByRole("heading", {
+      name: /Mauro/i,
+    });
+
+    expect(previewName).toHaveTextContent("Mauro");
+  });
+});
