76652eff50e63ea7f9ac8abb24e74debbe43fb31
maurolps <boroengenharia@gmail.com>
2025-04-09 15:23:11 -0300

    test(Integration): clicking on a card should toggle its expanded state

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,11 +1,38 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import App from "../App";
 import "@testing-library/jest-dom";
 
 describe("Card Collapse Behavior", () => {
-  test("Initially, Personal Details should be expanded", () => {
+  beforeEach(() => {
     render(<App />);
+  });
+
+  test("Initially, Personal Details should be expanded", () => {
     const personalExpandedCard = screen.getByPlaceholderText(/Phone Number/i);
     expect(personalExpandedCard).toBeInTheDocument();
+  });
+
+  test("Clicking on a card should toggle its expanded state", async () => {
+    const user = userEvent.setup();
+    const personalExpandedCard = screen.queryByPlaceholderText(/Phone Number/i);
+    const workCard = screen.getByRole("button", {
+      name: /Work Experience/i,
+    });
+    const workCollapsedCard = screen.queryByRole("button", {
+      name: /New Item/i,
+    });
+
+    expect(personalExpandedCard).toBeInTheDocument();
+    expect(workCollapsedCard).not.toBeInTheDocument();
+
+    await user.click(workCard);
+
+    const workExpandedCard = await screen.findByRole("button", {
+      name: /New Item/i,
+    });
+
+    expect(personalExpandedCard).not.toBeInTheDocument();
+    expect(workExpandedCard).toBeInTheDocument();
   });
 });
